a3e38cf0c2658435c10f20310b468e9920aaa05d
mitani1993 <fytim55@gmail.com>
2021-12-12 17:18:28 +0900

    Initial commit

---
+++ pomodoro.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require './timer_base'
+
+# ポモドーロクラス
+class Pomodoro < TimerBase
+  MINUTES = 25
+
+  def initialize(minutes: MINUTES)
+    super
+  end
+end
---
+++ test/test_timer_base.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative '../timer_base'
+
+timer = TimerBase.new(minutes: 1)
+timer.run
